3a6465deb90051e0ff4c1357d3a3ebf7bdb193df
Matheus <reboucas444@gmail.com>
2025-10-09 14:54:03 -0300

    placing an icon in the header and making it responsive by placing a heart mask

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,19 +1,25 @@
 import React from "react";
+import Icon from "../../assets/icons/icon.png";
 import "./Header.css";
 
 export default function Header() {
   const names = "Bia & Matheus";
   const startDates = "17.10.1017";
+  const Iconheart = Icon;
+
   return (
     <>
       <header className="header-app">
-        <div>
-          <h1 className="header-title">{names}</h1>
-          <p className="header-subtitle">Juntos desde {startDates}</p>
+        <div className="header-logo-div">
+          <img className="icon-logo" src={Iconheart} alt="Icone Coração" />
+          <div>
+            <h1 className="header-title">{names}</h1>
+            <p className="header-subtitle">Juntos desde {startDates}</p>
+          </div>
         </div>
         <div>
-          <p className="header-message0">Onde tudo começou</p>
-          <p className="header-message1">Nosso tempo nunca para de crescer!</p>
+          <p className="header-message0">8 anos</p>
+          <p className="header-message1">Celebrando nossa história.</p>
         </div>
       </header>
     </>
